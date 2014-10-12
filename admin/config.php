28b7a8c4820be3ac5a972ebff51f9c2532308237
Kuntau <kuntau17@gmail.com>
2014-10-13 03:14:23 +0800

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.0.110/cart/admin/');
+define('HTTP_CATALOG', 'http://192.168.0.110/cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.0.110/cart/admin/');
+define('HTTPS_CATALOG', 'http://192.168.0.110/cart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/cart/admin/');
+define('DIR_SYSTEM', '/var/www/html/cart/system/');
+define('DIR_LANGUAGE', '/var/www/html/cart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/cart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/cart/system/config/');
+define('DIR_IMAGE', '/var/www/html/cart/image/');
+define('DIR_CACHE', '/var/www/html/cart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/cart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/cart/system/upload/');
+define('DIR_LOGS', '/var/www/html/cart/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/cart/system/modification/');
+define('DIR_CATALOG', '/var/www/html/cart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kuntau');
+define('DB_PASSWORD', 'kunkun');
+define('DB_DATABASE', 'opencart_20');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.0.110/cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.0.110/cart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/cart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/cart/system/');
+define('DIR_LANGUAGE', '/var/www/html/cart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/cart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/cart/system/config/');
+define('DIR_IMAGE', '/var/www/html/cart/image/');
+define('DIR_CACHE', '/var/www/html/cart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/cart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/cart/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/cart/system/modification/');
+define('DIR_LOGS', '/var/www/html/cart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kuntau');
+define('DB_PASSWORD', 'kunkun');
+define('DB_DATABASE', 'opencart_20');
+define('DB_PREFIX', 'oc_');
