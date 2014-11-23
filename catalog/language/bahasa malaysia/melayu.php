609fa6948de89454457e4a67ab3fc373fe9307d1
Kuntau <kuntau17@gmail.com>
2014-11-23 17:53:35 +0800

    try rename file

---
+++ catalog/language/bahasa malaysia/melayu.php
@@ -0,0 +1,73 @@
+﻿<?php
+// Locale
+$_['code']                  = 'my';
+$_['charset']               = 'utf-8';
+$_['direction']             = 'ltr';
+$_['locale']                = 'my_MY.UTF-8,ms,ms-MY,malay';
+$_['date_format_short']     = 'd/m/Y';
+$_['date_format_long']      = 'l dS F Y';
+$_['time_format']           = 'h:i:s A';
+$_['datetime_format']       = 'd/m/Y H:i:s';
+$_['decimal_point']         = '.';
+$_['thousand_point']        = ',';
+
+// Text
+$_['text_home']             = '<i class="fa fa-home"></i>';
+$_['text_yes']              = 'Ya';
+$_['text_no']               = 'Tidak';
+$_['text_none']             = ' --- Tiada --- ';
+$_['text_select']           = ' --- Sila Pilih --- ';
+$_['text_all_zones']        = 'Semua Zon';
+$_['text_pagination']       = 'Papar dari %d ke %d daripada %d (%d Halaman)';
+$_['text_separator']        = ' &raquo; ';
+$_['text_loading']          = 'Loading...';
+
+// Buttons
+$_['button_address_add']    = 'Tambah Alamat';
+$_['button_back']           = 'Kembali';
+$_['button_continue']       = 'Teruskan';
+$_['button_cart']           = 'Tambah ke Troli';
+$_['button_cancel']         = 'Batal';
+$_['button_compare']        = 'Tambah untuk Bandingkan';
+$_['button_wishlist']       = 'Tambah ke Senarai Permintaaan';
+$_['button_checkout']       = 'Checkout';
+$_['button_confirm']        = 'Sahkan Pesanan';
+$_['button_coupon']         = 'Gunakan Kupon';
+$_['button_delete']         = 'Padam';
+$_['button_download']       = 'Muatturun';
+$_['button_edit']           = 'Tukar';
+$_['button_filter']         = 'Perbaiki carian';
+$_['button_new_address']    = 'Alamat Baru';
+$_['button_change_address'] = 'Tukar Alamat';
+$_['button_add_product']    = 'Tambah Produk';
+$_['button_reviews']        = 'Ulasan';
+$_['button_write']          = 'Tulis Ulasan';
+$_['button_login']          = 'Login';
+$_['button_update']         = 'Kemaskini';
+$_['button_remove']         = 'Remove';
+$_['button_reorder']        = 'Reorder';
+$_['button_return']         = 'Return';
+$_['button_shopping']       = 'Teruskan Pembelian';
+$_['button_search']         = 'Carian';
+$_['button_shipping']       = 'Penghantaran';
+$_['button_submit']         = 'Hantar';
+$_['button_guest']          = 'Guest Checkout';
+$_['button_view']           = 'Papar';
+$_['button_voucher']        = 'Gunakan Voucher';
+$_['button_upload']         = 'Muatnaik Fail';
+$_['button_reward']         = 'Gunakan Mata Ganjaran';
+$_['button_quote']          = 'Dapatkan Sebutharga';
+$_['button_list']           = 'List';
+$_['button_grid']           = 'Grid';
+$_['button_map']            = 'Lihat Google Map';
+
+// Error
+$_['error_exception']       = 'Kod Ralat(%s): %s di %s pada baris %s';
+$_['error_upload_1']        = 'Amaran: Fail dimuatnaik melebihi saiz max fail dibenarkan (rujuk : upload_max_filesize directive dalam php.ini)!';
+$_['error_upload_2']        = 'Amaran: Fail dimuatnaik melebihi saiz max fail dibenarkan (rujuk MAX_FILE_SIZE directive dalam the HTML form)!';
+$_['error_upload_3']        = 'Amaran: Hanya sebahagian fail berjaya dimuatnaik!';
+$_['error_upload_4']        = 'Amaran: Tiada fail dimuatnaik!';
+$_['error_upload_6']        = 'Amaran: Folder "temporary" tiada dijumpai!';
+$_['error_upload_7']        = 'Amaran: Gagal tulis fail ke disk!';
+$_['error_upload_8']        = 'Amaran: Fail muatnaik dibatalkan oleh "extension"!';
+$_['error_upload_999']      = 'Amaran: Kod ralat tidak difahami!';
